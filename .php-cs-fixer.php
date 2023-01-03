7a14cb6cc3695eb9ac4f897a63c0d6f12b79c33b
David Johnson <david.johnson@fidelitybanknc.com>
2023-01-03 09:35:56 -0500

    .

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,9 +6,9 @@ require __DIR__ . '/vendor/autoload.php';
 
 return (new Config())
     ->setRules([
-        '@PhpCsFixer' => true,
+        '@PhpCsFixer' => true, // <-- removing this line will fix the issue
         'no_alternative_syntax' => [
-            'fix_non_monolithic_code' => true
+            'fix_non_monolithic_code' => false
         ],
     ])
     ->setIndent('    ')
